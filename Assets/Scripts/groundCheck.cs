ef605de23eaa38ff37633b3dfe3805bb905507c2
Y Sel Ayun <yselayun@gmail.com>
2020-11-25 16:23:54 +0700

    Sua Animation

---
+++ Assets/Scripts/groundCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Player player;
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void FixedUpdate()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        player._falled = false;
+        player._grounded = true;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        player._falled = false;
+        player._grounded = true;
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        player._grounded = false;
+    }
+}
